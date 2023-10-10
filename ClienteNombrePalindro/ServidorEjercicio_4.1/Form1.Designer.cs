6853cf39361f4fcb177f7b8eefb0c15da2f613d9
Pedrorodriguez016 <146534432+Pedrorodriguez016@users.noreply.github.com>
2023-10-10 16:55:05 +0200

    Ejercicio

---
+++ ClienteNombrePalindro/ServidorEjercicio_4.1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace ServidorEjercicio_4._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ejecutar = new System.Windows.Forms.Button();
+            this.Datos = new System.Windows.Forms.TextBox();
+            this.NombrePalindro = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // Ejecutar
+            // 
+            this.Ejecutar.Location = new System.Drawing.Point(278, 278);
+            this.Ejecutar.Name = "Ejecutar";
+            this.Ejecutar.Size = new System.Drawing.Size(193, 70);
+            this.Ejecutar.TabIndex = 0;
+            this.Ejecutar.Text = "Iniciar";
+            this.Ejecutar.UseVisualStyleBackColor = true;
+            this.Ejecutar.Click += new System.EventHandler(this.Ejecutar_Click);
+            // 
+            // Datos
+            // 
+            this.Datos.Location = new System.Drawing.Point(137, 82);
+            this.Datos.Name = "Datos";
+            this.Datos.Size = new System.Drawing.Size(160, 26);
+            this.Datos.TabIndex = 1;
+            // 
+            // NombrePalindro
+            // 
+            this.NombrePalindro.AutoSize = true;
+            this.NombrePalindro.Location = new System.Drawing.Point(137, 154);
+            this.NombrePalindro.Name = "NombrePalindro";
+            this.NombrePalindro.Size = new System.Drawing.Size(329, 24);
+            this.NombrePalindro.TabIndex = 2;
+            this.NombrePalindro.TabStop = true;
+            this.NombrePalindro.Text = "Saber si el nombre introducido es palindro";
+            this.NombrePalindro.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NombrePalindro);
+            this.Controls.Add(this.Datos);
+            this.Controls.Add(this.Ejecutar);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ejecutar;
+        private System.Windows.Forms.TextBox Datos;
+        private System.Windows.Forms.RadioButton NombrePalindro;
+    }
+}
+
---
+++ ClienteNombrePalindro/ServidorEjercicio_4.1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ServidorEjercicio_4._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Ejecutar_Click(object sender, EventArgs e)
+        {
+            Socket server;
+            //Crear el IPEndPoint de nuestro cliente al servidor por el puerto al que nos queremos conectar
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9051);
+
+            //crear el socket
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+                this.BackColor = Color.Green;
+                if (NombrePalindro.Checked)
+                {
+                    string nombre = "1/" + Datos.Text;
+                    //enviamos al servidor el nombre introducido
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(nombre);
+                    server.Send(msg);
+
+                    //recibimos el resultado
+                    byte[] msg2 = new byte[512];
+                    server.Receive(msg2);
+                    nombre = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                    if (nombre == "NO")
+                        MessageBox.Show("Tu nombre no es palindro");
+                    else
+                        MessageBox.Show("El nombre"  +nombre+  " es palindro");
+
+                }
+                
+                this.BackColor = Color.Gray;
+                server.Shutdown(SocketShutdown.Both);
+                server.Close();
+            }
+            catch(SocketException)
+            {
+                MessageBox.Show("No se pudo conectar al servidor");
+            }
+
+
+        }
+    }
+}
