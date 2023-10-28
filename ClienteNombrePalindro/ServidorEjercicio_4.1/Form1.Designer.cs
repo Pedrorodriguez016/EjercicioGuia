e75fb4776d32c791c75aa3b3ea4485cfe7817263
Pedrorodriguez016 <146534432+Pedrorodriguez016@users.noreply.github.com>
2023-10-28 12:16:59 +0200

    Version con conexion-desconexion

--- ClienteNombrePalindro/ServidorEjercicio_4.1/Form1.Designer.cs
+++ ClienteNombrePalindro/ServidorEjercicio_4.1/Form1.Designer.cs
@@ -30,8 +30,16 @@ namespace ServidorEjercicio_4._1
         private void InitializeComponent()
         {
             this.Ejecutar = new System.Windows.Forms.Button();
-            this.Datos = new System.Windows.Forms.TextBox();
+            this.nombre = new System.Windows.Forms.TextBox();
             this.NombrePalindro = new System.Windows.Forms.RadioButton();
+            this.Bonito = new System.Windows.Forms.RadioButton();
+            this.Longitud = new System.Windows.Forms.RadioButton();
+            this.Altura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.alto = new System.Windows.Forms.RadioButton();
+            this.Conectar = new System.Windows.Forms.Button();
+            this.Desconectar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Ejecutar
@@ -44,12 +52,12 @@ namespace ServidorEjercicio_4._1
             this.Ejecutar.UseVisualStyleBackColor = true;
             this.Ejecutar.Click += new System.EventHandler(this.Ejecutar_Click);
             // 
-            // Datos
+            // nombre
             // 
-            this.Datos.Location = new System.Drawing.Point(137, 82);
-            this.Datos.Name = "Datos";
-            this.Datos.Size = new System.Drawing.Size(160, 26);
-            this.Datos.TabIndex = 1;
+            this.nombre.Location = new System.Drawing.Point(137, 82);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(160, 26);
+            this.nombre.TabIndex = 1;
             // 
             // NombrePalindro
             // 
@@ -62,13 +70,99 @@ namespace ServidorEjercicio_4._1
             this.NombrePalindro.Text = "Saber si el nombre introducido es palindro";
             this.NombrePalindro.UseVisualStyleBackColor = true;
             // 
+            // Bonito
+            // 
+            this.Bonito.AutoSize = true;
+            this.Bonito.Location = new System.Drawing.Point(137, 194);
+            this.Bonito.Name = "Bonito";
+            this.Bonito.Size = new System.Drawing.Size(177, 24);
+            this.Bonito.TabIndex = 3;
+            this.Bonito.TabStop = true;
+            this.Bonito.Text = "Mi nombre es bonito";
+            this.Bonito.UseVisualStyleBackColor = true;
+            // 
+            // Longitud
+            // 
+            this.Longitud.AutoSize = true;
+            this.Longitud.Location = new System.Drawing.Point(137, 236);
+            this.Longitud.Name = "Longitud";
+            this.Longitud.Size = new System.Drawing.Size(179, 24);
+            this.Longitud.TabIndex = 4;
+            this.Longitud.TabStop = true;
+            this.Longitud.Text = "Longitud del nombre";
+            this.Longitud.UseVisualStyleBackColor = true;
+            // 
+            // Altura
+            // 
+            this.Altura.Location = new System.Drawing.Point(509, 81);
+            this.Altura.Name = "Altura";
+            this.Altura.Size = new System.Drawing.Size(100, 26);
+            this.Altura.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(509, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Altura";
+            // 
+            // alto
+            // 
+            this.alto.AutoSize = true;
+            this.alto.Location = new System.Drawing.Point(544, 154);
+            this.alto.Name = "alto";
+            this.alto.Size = new System.Drawing.Size(144, 24);
+            this.alto.TabIndex = 8;
+            this.alto.TabStop = true;
+            this.alto.Text = "Dime si soy alto";
+            this.alto.UseVisualStyleBackColor = true;
+            // 
+            // Conectar
+            // 
+            this.Conectar.Location = new System.Drawing.Point(66, 372);
+            this.Conectar.Name = "Conectar";
+            this.Conectar.Size = new System.Drawing.Size(181, 50);
+            this.Conectar.TabIndex = 9;
+            this.Conectar.Text = "Conectar";
+            this.Conectar.UseVisualStyleBackColor = true;
+            this.Conectar.Click += new System.EventHandler(this.Conectar_Click);
+            // 
+            // Desconectar
+            // 
+            this.Desconectar.Location = new System.Drawing.Point(484, 372);
+            this.Desconectar.Name = "Desconectar";
+            this.Desconectar.Size = new System.Drawing.Size(204, 50);
+            this.Desconectar.TabIndex = 10;
+            this.Desconectar.Text = "Desconectar";
+            this.Desconectar.UseVisualStyleBackColor = true;
+            this.Desconectar.Click += new System.EventHandler(this.Desconectar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Desconectar);
+            this.Controls.Add(this.Conectar);
+            this.Controls.Add(this.alto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Altura);
+            this.Controls.Add(this.Longitud);
+            this.Controls.Add(this.Bonito);
             this.Controls.Add(this.NombrePalindro);
-            this.Controls.Add(this.Datos);
+            this.Controls.Add(this.nombre);
             this.Controls.Add(this.Ejecutar);
             this.Name = "Form1";
             this.Text = " ";
@@ -80,8 +174,16 @@ namespace ServidorEjercicio_4._1
         #endregion
 
         private System.Windows.Forms.Button Ejecutar;
-        private System.Windows.Forms.TextBox Datos;
+        private System.Windows.Forms.TextBox nombre;
         private System.Windows.Forms.RadioButton NombrePalindro;
+        private System.Windows.Forms.RadioButton Bonito;
+        private System.Windows.Forms.RadioButton Longitud;
+        private System.Windows.Forms.TextBox Altura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton alto;
+        private System.Windows.Forms.Button Conectar;
+        private System.Windows.Forms.Button Desconectar;
     }
 }
 
--- ClienteNombrePalindro/ServidorEjercicio_4.1/Form1.cs
+++ ClienteNombrePalindro/ServidorEjercicio_4.1/Form1.cs
@@ -15,52 +15,119 @@ namespace ServidorEjercicio_4._1
 {
     public partial class Form1 : Form
     {
+        Socket server;
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Ejecutar_Click(object sender, EventArgs e)
-        {
-            Socket server;
-            //Crear el IPEndPoint de nuestro cliente al servidor por el puerto al que nos queremos conectar
-            IPAddress direc = IPAddress.Parse("192.168.56.102");
-            IPEndPoint ipep = new IPEndPoint(direc, 9051);
+        { 
+            if(Longitud.Checked)
+            {
 
-            //crear el socket
+            string mensaje = "1/" + nombre.Text;
+            // Enviamos al servidor el nombre tecleado
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+            MessageBox.Show("La longitud de tu nombre es: " + mensaje);
+            }
+            else if (Bonito.Checked)
+            {
+                string mensaje = "2/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+
+                if (mensaje == "SI")
+                    MessageBox.Show("Tu nombre ES bonito.");
+                else
+                    MessageBox.Show("Tu nombre NO bonito. Lo siento.");
+
+            }
+            if (NombrePalindro.Checked)
+            {
+                string mensaje = "4/" + nombre.Text;
+                //enviamos al servidor el nombre introducido
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //recibimos el resultado
+                byte[] msg2 = new byte[512];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                if (mensaje == "NO")
+                    MessageBox.Show("Tu nombre no es palindro");
+                else
+                    MessageBox.Show("El nombre" + nombre.Text.ToUpper() + " es palindro");
+
+            }
+            else
+            {
+                // Enviamos nombre y altura
+                string mensaje = "3/" + nombre.Text + "/" + Altura.Text;
+               // Enviamos al servidor el nombre tecleado
+               byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+               server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show(mensaje);
+            }
+
+
+        }
+
+        private void Conectar_Click(object sender, EventArgs e)
+        {
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+
+            //Creamos el socket 
             server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
-                server.Connect(ipep);
+                server.Connect(ipep);//Intentamos conectar el socket
                 this.BackColor = Color.Green;
-                if (NombrePalindro.Checked)
-                {
-                    string nombre = "1/" + Datos.Text;
-                    //enviamos al servidor el nombre introducido
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(nombre);
-                    server.Send(msg);
+                MessageBox.Show("Conectado");
 
-                    //recibimos el resultado
-                    byte[] msg2 = new byte[512];
-                    server.Receive(msg2);
-                    nombre = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-                    if (nombre == "NO")
-                        MessageBox.Show("Tu nombre no es palindro");
-                    else
-                        MessageBox.Show("El nombre"  +nombre+  " es palindro");
-
-                }
-                
-                this.BackColor = Color.Gray;
-                server.Shutdown(SocketShutdown.Both);
-                server.Close();
             }
-            catch(SocketException)
+            catch (SocketException ex)
             {
-                MessageBox.Show("No se pudo conectar al servidor");
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
             }
+        }
 
+        private void Desconectar_Click(object sender, EventArgs e)
+        {
+            //Mensaje de desconexión
+            string mensaje = "0/";
 
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            // Nos desconectamos
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
         }
     }
 }
